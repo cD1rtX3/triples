907bea0ebb417c47fbd41db425c7043040dae016
Coarse Rosinflower <113859223+cD1rtX3@users.noreply.github.com>
2025-03-05 11:19:39 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 export default class Main {
 	static ctx = document.getElementById("canvas").getContext("2d");
-	static main(e) {
-		document.getElementsByClassName("script-required").item(0).style = "";
-		Main.resize(undefined);
-		window.addEventListener("resize", Main.resize);
-	}
 	static resize(e) {
 		const canv = document.getElementById("canvas");
 		const r = canv.getBoundingClientRect();
 		const dim = Math.min(window.innerWidth * 0.5625 - r.left, window.innerHeight - r.top) - 16;
-		canv.style = `width: ${16 / 9 * dim}px; height: ${dim}px`;
+		canv.style = `width: ${String(Math.floor(16 / 9 * dim))}px; height: ${String(Math.floor(dim))}px`;
+	}
+	static init() {
+		//
+	}
+	static main(e) {
+		document.getElementsByClassName("script-required").item(0).style = "";
+		Main.resize(undefined);
+		window.addEventListener("resize", Main.resize);
+		Main.init();
 	}
 }
