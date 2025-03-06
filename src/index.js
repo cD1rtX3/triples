0e096315de8d279aff8793960065891fcef6b599
Coarse Rosinflower <113859223+cD1rtX3@users.noreply.github.com>
2025-03-06 12:48:50 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export default class Main {
 	static ctx = document.getElementById("canvas").getContext("2d");
+	static dark;
 	static resize(e) {
 		const canv = document.getElementById("canvas");
 		const r = canv.getBoundingClientRect();
@@ -7,12 +8,14 @@ export default class Main {
 		canv.style = `width: ${String(Math.floor(16 / 9 * dim))}px; height: ${String(Math.floor(dim))}px`;
 	}
 	static init() {
-		//
+		ctx.fillStyle = /* Main.dark ? "black" : */ "#efb";
+		ctx.fillRect(0, 0, 1280, 720);
 	}
 	static main(e) {
 		document.getElementsByClassName("script-required").item(0).style = "";
 		Main.resize(undefined);
 		window.addEventListener("resize", Main.resize);
+		Main.dark = window.matchMedia?.('(prefers-color-scheme: dark)').matches;
 		Main.init();
 	}
 }
